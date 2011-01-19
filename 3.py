ef17bc8ba64b384b68c513ebab0517ac9ad13a96
rmdrimmie <rob.drimmie@gmail.com>
2011-01-18 21:24:39 -0500

    Ooookay, that is really not working. There is a better way of determining a prime factor, and I have no idea what it is so I will have to do me some larnin'

--- 3.py
+++ 3.py
@@ -1,14 +1,20 @@
-#bignum = 600851475143
-bignum = 13195
+bignum = 600851475143
+#bignum = 13195
 working = bignum
+primefactors = []
 factor = 1
 largest = 1
 while factor < working: 
     if bignum % factor == 0:
-            largest = factor
-            print 'Found factor: %s' % (largest)
-            working = bignum / factor
-            print 'working now: %s' % (working)
+        largest = factor
+        print 'Found factor: %s' % (largest)
+        
+        if factor not in primefactors:
+            primefactors.append( factor )
+
+        
+        #working = bignum / factor
+        #print 'working now: %s' % (working)
     factor += 1
 
-print largest
+print primefactors 
