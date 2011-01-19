6092e7971a1dd83262f3808fca5f3dfac1064766
rmdrimmie <rob.drimmie@gmail.com>
2011-01-18 22:47:33 -0500

    I don't know if I was punching in the answer wrong or what, but it finally accepted the output of this as correct (well, the last element in the list that is output but!)

--- 3.py
+++ 3.py
@@ -1,19 +1,24 @@
 def is_prime( bignum ):
+  print bignum
   factor = 2
   while factor * factor < bignum:
       if bignum % factor == 0:
           return False
-
+      factor += 1
+  return True
 
 bignum = 600851475143
 #bignum = 13195d
+print is_prime(59569) 
 primefactors = []
 factor = 2
-while factor * factor < bignum: 
-    if is_prime(factor):
-      if bignum % factor == 0:
+working = bignum
+while factor < working: 
+    if bignum % factor == 0:
+      if is_prime( factor ):
         primefactors.append( factor )
-        
+        working = bignum / factor
+        print "Working %s", (working)
     factor += 1
 
 print primefactors 
