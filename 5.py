fe546c1af5ca3ecfa617e313285835242235767f
rmdrimmie <rob.drimmie@gmail.com>
2011-02-10 21:07:40 -0500

    got the repetitive factor removal thing done.

--- 5.py
+++ 5.py
@@ -1,8 +1,14 @@
 # project euler project 5
 # http://projecteuler.net/index.php?section=problems&id=5
 
-upper = 1
-for factor in range(1, 20):
-    upper *= factor
+factors = range( 20, 0, -1 )
 
-print "%s" % (upper )
+# remove factors that are even divisors of larger ones.
+for factor in factors:
+    for divisor in range( 1, factor ):
+        if divisor in factors:
+            if factor % divisor == 0:
+                factors.remove( divisor )
+
+print factors
+
